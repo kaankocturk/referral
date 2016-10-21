b382ca3045ab3e474050e81fc753e0bfcbe04421
kaankocturk <kkocturk1@babson.edu>
2016-10-21 14:50:54 +0300

    imgcompression

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
